c28b2875a48aa05c61268c0bf260cd1fede16cba
Dax Martineau <dkmarti9@asu.edu>
2019-10-08 23:35:21 -0700

    Added substance to all methods so they operate as required for assignment. Added more javadoc statements.

--- CSE360WorkSpace/src/cse360assign2/AddingMachine.java
+++ CSE360WorkSpace/src/cse360assign2/AddingMachine.java
@@ -1,34 +1,80 @@
 package cse360assign2;
 
+import java.util.ArrayList;
+
+/**
+ * Assignment 2: Adding Machine Class
+ * <p>
+ * Class Id: 70641
+ * <p>
+ * Program to add or subtract inputs and then print
+ * out the formula used to find total.
+ * @author Dax Martineau
+ * 
+ */
+
 public class AddingMachine {
 
-	public static void main(String[] args) {
-	
-	}
 	private int total;
+	private ArrayList<String> addingMachineTracker = new ArrayList<String>();
 	
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+	
+	/**
+	 * Constructor for class AddingMachine.
+	 */
+	public AddingMachine() {
+		total = 0;
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	
+	/**
+	 * Adds an integer value to the total and saves the value in an ArrayList.
+	 * @param value An integer value to be added to the total value;
+	 */
 	public void add (int value) {
+		String valueAdder;
+		valueAdder = " + " +value;
+		addingMachineTracker.add(valueAdder);
+		
+		total = total + value;
 		
 	}
 	
+	
+	/**
+	 * Subtracts an integer value from the total and saves the value in an ArrayList.
+	 * @param value An integer value to be subtracted from the total value;
+	 */
 	public void subtract (int value) {
+		String valueAdder;
+		valueAdder = " - " +value;
+		addingMachineTracker.add(valueAdder);
+		
+		total = total - value;
 		
 	}
-		
+	
+	
 	public String toString () {
-		return "";
+		String totalString = "0";
+		for(int i = 0; i<addingMachineTracker.size(); ++i) {
+			totalString = totalString + addingMachineTracker.get(i);
+		}
+		
+		return totalString;
 	}
 
-	public void clear() {
 	
+	/**
+	 * Removes all elements in the ArrayList and sets the total equal to 0.
+	 */
+	public void clear() {
+		addingMachineTracker.clear();
+		total = 0;
 	}
 
 }
